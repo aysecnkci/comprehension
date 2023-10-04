07ccd84e702d24381d87996dfc215ebf172bab0f
ayse <aysecnkci@gmail.com>
2023-10-04 23:39:48 +0300

    new rule

---
+++ 2.hafta/rule_based_segment.py
@@ -0,0 +1,25 @@
+
+# EDA for Titanic Dataset
+
+
+
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset("titanic")
+
+df.head()
+
+df.columns
+
+df["embarked"].unique()
+
+
+sns.boxplot(x=df["age"])
+plt.show(block=True)
+
+
+
+df.describe().T
